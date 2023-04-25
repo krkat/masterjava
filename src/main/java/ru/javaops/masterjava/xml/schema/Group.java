4d552e9cdca6a81d734148995b61c6019fd84385
EmmaFranBengtsbo <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2023-04-25 22:02:30 +0300

    HW02_02 Generate java code from xsd using JAXB

---
+++ src/main/java/ru/javaops/masterjava/xml/schema/Group.java
@@ -0,0 +1,187 @@
+
+package ru.javaops.masterjava.xml.schema;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="type" type="{http://javaops.ru}groupType"/>
+ *         &lt;element name="Users">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;element ref="{http://javaops.ru}User"/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "name",
+    "type",
+    "users"
+})
+@XmlRootElement(name = "Group", namespace = "http://javaops.ru")
+public class Group {
+
+    @XmlElement(namespace = "http://javaops.ru", required = true)
+    protected String name;
+    @XmlElement(namespace = "http://javaops.ru", required = true)
+    @XmlSchemaType(name = "string")
+    protected GroupType type;
+    @XmlElement(name = "Users", namespace = "http://javaops.ru", required = true)
+    protected Group.Users users;
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the type property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link GroupType }
+     *     
+     */
+    public GroupType getType() {
+        return type;
+    }
+
+    /**
+     * Sets the value of the type property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link GroupType }
+     *     
+     */
+    public void setType(GroupType value) {
+        this.type = value;
+    }
+
+    /**
+     * Gets the value of the users property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Group.Users }
+     *     
+     */
+    public Group.Users getUsers() {
+        return users;
+    }
+
+    /**
+     * Sets the value of the users property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Group.Users }
+     *     
+     */
+    public void setUsers(Group.Users value) {
+        this.users = value;
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;element ref="{http://javaops.ru}User"/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "user"
+    })
+    public static class Users {
+
+        @XmlElement(name = "User", namespace = "http://javaops.ru", required = true)
+        protected User user;
+
+        /**
+         * Gets the value of the user property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link User }
+         *     
+         */
+        public User getUser() {
+            return user;
+        }
+
+        /**
+         * Sets the value of the user property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link User }
+         *     
+         */
+        public void setUser(User value) {
+            this.user = value;
+        }
+
+    }
+
+}
---
+++ src/main/java/ru/javaops/masterjava/xml/schema/Project.java
@@ -0,0 +1,185 @@
+
+package ru.javaops.masterjava.xml.schema;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="description" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="Groups">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;element ref="{http://javaops.ru}Group"/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "name",
+    "description",
+    "groups"
+})
+@XmlRootElement(name = "Project", namespace = "http://javaops.ru")
+public class Project {
+
+    @XmlElement(namespace = "http://javaops.ru", required = true)
+    protected String name;
+    @XmlElement(namespace = "http://javaops.ru", required = true)
+    protected String description;
+    @XmlElement(name = "Groups", namespace = "http://javaops.ru", required = true)
+    protected Project.Groups groups;
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the description property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the value of the description property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+    /**
+     * Gets the value of the groups property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Project.Groups }
+     *     
+     */
+    public Project.Groups getGroups() {
+        return groups;
+    }
+
+    /**
+     * Sets the value of the groups property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Project.Groups }
+     *     
+     */
+    public void setGroups(Project.Groups value) {
+        this.groups = value;
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;element ref="{http://javaops.ru}Group"/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "group"
+    })
+    public static class Groups {
+
+        @XmlElement(name = "Group", namespace = "http://javaops.ru", required = true)
+        protected Group group;
+
+        /**
+         * Gets the value of the group property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link Group }
+         *     
+         */
+        public Group getGroup() {
+            return group;
+        }
+
+        /**
+         * Sets the value of the group property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link Group }
+         *     
+         */
+        public void setGroup(Group value) {
+            this.group = value;
+        }
+
+    }
+
+}
